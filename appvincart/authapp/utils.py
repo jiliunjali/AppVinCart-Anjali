8ef220b32e9e7e70ae2a6d94a465e6417f7b4773
jiliunjali <anjali20012910@gmail.com>
2024-02-12 23:56:11 +0530

    commit 12th 4

--- appvincart/authapp/utils.py
+++ appvincart/authapp/utils.py
@@ -20,19 +20,3 @@ class EmailUtils:
             server.starttls()
             server.login(sender_email, password)
             server.sendmail(sender_email, recipient_email, message.as_string())
-
-
-# from django.core.mail import EmailMessage
-# import os
-
-# class Util:
-#     @staticmethod
-#     def send_email(data):
-#         email = EmailMessage(
-#             subject = data['email_subject'],
-#             body = data['body'],
-#             from_email = os.environ.get('EMAIL_FROM'),
-#             to = [data['to_email']]
-#         )
-#         email.send()
-# utils.py
